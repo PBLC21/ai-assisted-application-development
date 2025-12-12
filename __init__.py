561891cd26b4c93c8ddc38af0bbda5b8b5c25902
Jorge B. Perez <jperezb65@gmail.com>
2025-12-12 12:42:59 -0600

    'delete files'

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-TEKS Standards Module
-Provides API endpoints for querying Texas Essential Knowledge and Skills standards
-"""
-
-from .router import router
-from .service import teks_service
-
-__all__ = ["router", "teks_service"]
